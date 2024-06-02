db27233c5f09340317d2930ca271ceea1c21eafb
Santosh Shrestha <santosh@rumsan.com>
2024-06-01 20:20:16 -0400

    updated code

--- play.ts
+++ play.ts
@@ -1,9 +1,17 @@
+//ffmpeg -hide_banner -loglevel error -y -i test_broadcast.mp3 -ac 1 -ar 8000 test_broadcast.wav
+
+//Redis
+// Check ports sudo lsof -i -P -n | grep LISTEN
+// Install using snap
+// sudo snap install redis
+// redis.cli
+
 import * as ariClient from "ari-client";
 import * as http from "http";
 
 // Create ARI client and start listening for events
 ariClient.connect(
-  "http://192.168.1.61:8088/ari",
+  "http://192.168.1.108:8088/ari",
   "rumsan",
   "temp9670",
   (err, client) => {
@@ -16,20 +24,22 @@ ariClient.connect(
 
     client.channels.originate(
       {
-        endpoint: "SIP/GOIP1/43+9779801109670",
-        //endpoint: "SIP/cellcentric/17772927336110",
-        //endpoint: "PJSIP/701",
+        //endpoint: "SIP/GOIP1/43+9779801109670",
+        //endpoint: "SIP/callcentric/13309670033",
+        endpoint: "PJSIP/701",
+        //endpoint: "SIP/0967009779845160080@sbc.anveo.com",
         //extension: "702",
         app: "app-name",
         context: "from-internal",
-        callerId: "Rumsan Test<9670>",
+        callerId: "Rumsan Test<+9779860341451>",
         priority: 1,
       },
       (err, channel) => {
         channel.on("StasisStart", (event) => {
           const playback1 = channel.play(
             {
-              media: "recording:congrats",
+              //media: "sound:/var/spool/asterisk/rumsan/raktim_santosh_demo",
+              media: "recording:bb56d70d-fba9-4d9e-9b00-656e7734339d"
             },
             playback,
             () => {
